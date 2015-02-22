8c0d67352d941a4cd4d6e6ceb2abf56c9178c066
David Koes <dkoes@pitt.edu>
2015-02-22 00:09:04 -0500

    more debug

--- scripts/createconfs.py
+++ scripts/createconfs.py
@@ -163,7 +163,7 @@ if __name__ == '__main__':
         except (KeyboardInterrupt, SystemExit):
             raise
         except Exception as e:
-            print e,smile
+            print e,smile,name
     
     #clear out queues
     for _ in xrange(numt):
