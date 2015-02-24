6ec0c45977df566c9643170adf8fcf68451674aa
David Koes <dkoes@pitt.edu>
2015-02-24 12:47:30 -0500

    add script for NSC compounds
    This version is awfully slow do to the need to make many single connections..
    but I don't want to deal with the horrible PUG XML interface

---
+++ scripts/nscsmiles.py
@@ -0,0 +1,23 @@
+#!/usr/local/bin/python
+
+#extract smiles for DTP/NCI compounds from PubChem with NSC identifiers
+
+import sys,urllib2,json
+
+sids = urllib2.urlopen('https://pubchem.ncbi.nlm.nih.gov/rest/pug/substance/sourceall/DTP.NCI/sids/TXT')
+
+#I couldn't figure out a why to get the NSC identifier and the isomeric smiels at the same time in one download
+
+for sidline in sids:
+    sid = sidline.strip()
+    sidjson = json.load(urllib2.urlopen('https://pubchem.ncbi.nlm.nih.gov/rest/pug/substance/sid/%s/JSON'%sid))
+    if len(sidjson) > 0:
+        record = sidjson['PC_Substances'][0]
+        nscid = 'NSC'+record['source']['db']['source_id']['str']
+        clist = record['compound']
+        for cl in clist:
+            if cl['id']['type'] == 'standardized':
+                cid = cl['id']['id']['cid']
+                smile = urllib2.urlopen('https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/%s/property/IsomericSMILES/TXT'%cid).read().strip()
+                print smile,nscid
+                
