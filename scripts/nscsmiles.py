c05736c6a1491c82a1054c0481c858043da5c2c9
David Koes <dkoes@porky.dcb.private.net>
2015-03-03 17:27:31 -0500

    nsc fixes

--- scripts/nscsmiles.py
+++ scripts/nscsmiles.py
@@ -4,20 +4,22 @@
 
 import sys,urllib2,json
 
-sids = urllib2.urlopen('https://pubchem.ncbi.nlm.nih.gov/rest/pug/substance/sourceall/DTP.NCI/sids/TXT')
+sids = urllib2.urlopen('https://pubchem.ncbi.nlm.nih.gov/rest/pug/substance/sourceall/DTP.NCI/sids/TXT').read().split('\n')
 
 #I couldn't figure out a why to get the NSC identifier and the isomeric smiels at the same time in one download
 
 for sidline in sids:
     sid = sidline.strip()
-    sidjson = json.load(urllib2.urlopen('https://pubchem.ncbi.nlm.nih.gov/rest/pug/substance/sid/%s/JSON'%sid))
-    if len(sidjson) > 0:
-        record = sidjson['PC_Substances'][0]
-        nscid = 'NSC'+record['source']['db']['source_id']['str']
-        clist = record['compound']
-        for cl in clist:
-            if cl['id']['type'] == 'standardized':
-                cid = cl['id']['id']['cid']
-                smile = urllib2.urlopen('https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/%s/property/IsomericSMILES/TXT'%cid).read().strip()
-                print smile,nscid
-                
+    try:
+		sidjson = json.load(urllib2.urlopen('https://pubchem.ncbi.nlm.nih.gov/rest/pug/substance/sid/%s/JSON'%sid))
+		if len(sidjson) > 0:
+			record = sidjson['PC_Substances'][0]
+			nscid = 'NSC'+record['source']['db']['source_id']['str']
+			clist = record['compound']
+			for cl in clist:
+				if cl['id']['type'] == 'standardized':
+					cid = cl['id']['id']['cid']
+					smile = urllib2.urlopen('https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/%s/property/IsomericSMILES/TXT'%cid).read().strip()
+					print smile,nscid
+    except:
+       sys.stderr.write('https://pubchem.ncbi.nlm.nih.gov/rest/pug/substance/sid/%s/JSON'%sid)        
