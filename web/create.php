8f2bf74f354f71015a49775c4a0d89765de1b3a4
Charles Yuan <Charles@Tydirium.local>
2015-03-16 14:02:37 -0400

    syntax

--- web/create.php
+++ web/create.php
@@ -226,7 +226,7 @@ else if(isset($_REQUEST["op"])) //operation
 				$stmt->store_result();
 				headerhtml();
 				echo('<span class="font-3">status</span><br>
-					<span class="font">view the status of your submitted jobs</span><br><br>')
+					<span class="font">view the status of your submitted jobs</span><br><br>');
 
 				if($stmt->num_rows > 0) { //have already created databases
 
